c72f73694105a092a7c3285e159c3ac508c96ac4
Jose Antonio Lopez Perez <jose.lppz03@gmail.com>
2024-04-16 10:30:25 +0200

    Initial project structure. Added  pdfs

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        String alphabeto = "abcdefghijklmnñopqrstuvwxyzABCDEFGHIJKLMNÑOPQRSTUVWXYZáéíóúÁÉÍÓÚ0123456789 ,.:!-¿?()";
+        String claveK = "UCBxHcl!6jñydiFoMB!vol!:da";
+        String msgCifrado = "cCTñScE((ÚHGfiXfúPhmqzgi:uÍCrzVtCc vHC:lJfMHovhb4xAi3VFKxfsp AGyv(YsáSWvHb¿iopÁKDxKcD5ÉnGofpJGÁP5mfA(lr ÁCrIhis¿6)o-vpHoCFnmAFs3veÍEJIRcd4pneGjCFzCrgKrz4kxeLGMñHiF!ÉñySBmvrPT?zfFh3ñuáCSñLnop6mCo:oFqTB4KICj3oá3sCxPqbñbuñLgwvzMr?DHE!gfiUsN!Z.njeCñp:(X8WP4CoJ4ñpaLEPOYkohgneKdBJAjUcxod43: Sk8mr:bB6AIIl(7JóBevAs53NiÚVPño,-35";
+        Vigenere coder = new Vigenere(alphabeto, claveK);
+        String msgOriginal = coder.decode(msgCifrado);
+    }
+}
---
+++ src/Vigenere.java
@@ -0,0 +1,15 @@
+public class Vigenere {
+    String alphabet;
+    String keyK;
+
+    public Vigenere(String alphabeto, String claveK) {
+        this.alphabet = alphabeto;
+        this.keyK = claveK;
+    }
+
+
+    public String decode(String msgCifrado) {
+        String result = "";
+        return result;
+    }
+}
